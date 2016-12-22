6c12884c2d7c39bd1df91185f5a6eb62e58ede0f
ph4r05 <ph4r05@gmail.com>
2016-12-22 13:55:15 +0100

    travis fix

--- assets/build-wheels.sh
+++ assets/build-wheels.sh
@@ -4,6 +4,9 @@ set -e -x
 # Install a system package required by our library
 yum install -y tpm-tools opencryptoki-devel openCryptoki-devel trousers-devel openssl-devel python-pip python-devel
 
+ls -las /io/
+find /io/
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
@@ -11,17 +14,21 @@ for PYBIN in /opt/python/*/bin; do
 done
 
 ls -las wheelhouse/
-rm -rf wheelhouse/argparse-*whl
-rm -rf wheelhouse/six-*whl
-rm -rf wheelhouse/pycparser-*.whl
-rm -rf wheelhouse/funcsigs-*.whl
-rm -rf wheelhouse/mock-*.whl
-rm -rf wheelhouse/pbr-*.whl
-rm -rf wheelhouse/*-none-any.whl
-ls -las wheelhouse/
+#rm -rf wheelhouse/argparse-*whl
+#rm -rf wheelhouse/six-*whl
+#rm -rf wheelhouse/pycparser-*.whl
+#rm -rf wheelhouse/funcsigs-*.whl
+#rm -rf wheelhouse/mock-*.whl
+#rm -rf wheelhouse/pbr-*.whl
+#rm -rf wheelhouse/*-none-any.whl
+#ls -las wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
+    if [[ $whl == *"-none-any.hwl" ]]; then
+        continue
+    fi
+
     echo "Audit wheel for $whl"
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
